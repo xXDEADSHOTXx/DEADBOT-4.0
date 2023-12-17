254a103da41b3d0d53dc483fb8b7e5c032274a92
xXDEADSHOTXx <153774084+xXDEADSHOTXx@users.noreply.github.com>
2023-12-17 18:27:55 +0100

    Update SLY_Permissioned_Account_Management.user.js

--- Permissioned_Account_Manager/SLY_Permissioned_Account_Management.user.js
+++ Permissioned_Account_Manager/SLY_Permissioned_Account_Management.user.js
@@ -6,9 +6,9 @@
 // @author       SLY
 // @match        https://labs.staratlas.com/
 // @require      https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/anchor-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/buffer-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/bs58-browserified.js
+// @require      https://raw.githubusercontent.com/xXDEADSHOTXx/DEADBOT-4.0/main/anchor-browserified.js
+// @require      https://raw.githubusercontent.com/xXDEADSHOTXx/DEADBOT-4.0/main/buffer-browserified.js
+// @require      https://raw.githubusercontent.com/xXDEADSHOTXx/DEADBOT-4.0/main/bs58-browserified.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=staratlas.com
 // @grant        GM_setValue
 // @grant        GM_getValue
